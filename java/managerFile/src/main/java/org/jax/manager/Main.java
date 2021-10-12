0b04fde40695e772dc5c758f20883f552a2e418e
hatrongvu13 <hatrongvu13@gmail.com>
2021-10-12 09:31:37 +0700

    update code upload file

--- java/managerFile/src/main/java/org/jax/manager/Main.java
+++ java/managerFile/src/main/java/org/jax/manager/Main.java
@@ -2,10 +2,14 @@ package org.jax.manager;
 
 import org.jax.manager.common.FileUtils;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Manager file");
 //        FileUtils.createFileTxt("demo.txt", "demo created file \n new line");
-        FileUtils.appendFileTxt("demo.txt", "the next text");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        FileUtils.appendFileTxt("demo.txt", input);
     }
 }
